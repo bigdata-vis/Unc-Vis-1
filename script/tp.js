d9a34c3997a3ec517b1fd0642449babeb66bf7e6
Sam <s.salisu@live.co.uk>
2016-04-06 11:06:14 +0100

    Added radial chart to show delay in stops over 24 hours

--- script/tp.js
+++ script/tp.js
@@ -8,7 +8,11 @@ var bdv = {};
 bdv.dataManager = function module() {
     var exports = {},
         dispatch = d3.dispatch('geoReady', 'dataReady', 'dataLoading'),
-        data;
+        data,
+        transitcrossfilter = crossfilter(),
+
+        location;
+
 
     exports.loadCsvData = function (_file, _cleaningFunc) {
         var loadCsv = d3.csv(_file);
@@ -24,6 +28,12 @@ bdv.dataManager = function module() {
             });
             data = _response;
 
+            transitcrossfilter.add(_response);
+
+            location = transitcrossfilter.dimension(function (d) {
+                return d.LOCATION;
+            });
+
             dispatch.dataReady(_response);
         });
     };
@@ -36,6 +46,52 @@ bdv.dataManager = function module() {
         d3.json(_file, function (_err, _data) {
             _callback(_data);
         })
+    };
+
+    exports.getCrossfilterSize = function () {
+        return transitcrossfilter.size()
+    };
+
+// Create a filterLocation method to filter stop data by location area.
+    exports.filterLocation = function (_locationArea) {
+
+// Get the longitudes of our bounding box, and construct an array from them.
+        var longitudes = [_locationArea[0][0], _locationArea[1][0]],
+            latitudes = [_locationArea[0][1], _locationArea[1][1]];
+
+        location.filterFunction(function (d) {
+            return d[0] >= longitudes[0] && d[0] <= longitudes[1] && d[1] >= latitudes[0] && d[1] <= latitudes[1];
+        });
+// Return all records within our bounding box.
+        return location.top(Infinity);
+    };
+
+    //Create a getDelays method to filter and aggregate the delay data by stop.
+    exports.getDelays = function (_locations) {
+        var delayCrossfilter = crossfilter(),
+        //Create a dimension by hour of the scheduled stop time.
+            stopTime = delayCrossfilter.dimension(function (d) {
+                return d.SCHEDULED.getHours();
+            });
+
+        //Group the dimension by hour, and reduce it by increasing the count for all delays greater than 1.
+        lateArrivalsByStopTime = stopTime.group().reduce(
+            function reduceAdd(p, v) {
+                return v.DELAY > 0 ? p + 1 : p + 0;
+            },
+            function reduceRemove(p, v) {
+                return 0;
+            },
+            function reduceInitial(p, v) {
+                return 0;
+            }
+        );
+
+        //Add our filtered locations to the crossfilter.
+        delayCrossfilter.add(_locations);
+
+        //Return an array contained the aggregated data, and the stopTime dimension.
+        return [stopTime, lateArrivalsByStopTime];
     };
 
     d3.rebind(exports, dispatch, 'on');
@@ -52,7 +108,7 @@ bdv.map = function module() {
         center,
         scale,
         size,
-        brush;
+        brush, x1, x2, y1, y2, brushX, brushY;
 
     function exports(_selection) {
         // Set svg equal to the selection that invokes this module.
@@ -69,7 +125,35 @@ bdv.map = function module() {
 
         // Set the path up using our projection defined above.
         path = path || d3.geo.path()
-            .projection(projection)
+            .projection(projection);
+
+        //Get the longitude of the top left corner of our map area.
+        long1 = projection.invert([0, 0])[0];
+        //Get the longitude of the top right corner of our map area.
+        long2 = projection.invert([width, 0])[0];
+
+        //Get the latitude of the top left corner of our map area.
+        lat1 = projection.invert([0, 0])[1];
+        //Get the latitude of the bottom left corner of our map area.
+        lat2 = projection.invert([width, height])[1];
+
+        //Create a linear scale generator for the x of our brush.
+        brushX = d3.scale.linear()
+            .range([0, size[0]])
+            .domain([long1, long2]);
+
+        //Create a linear scale generator for the y of our brush.
+        brushY = d3.scale.linear()
+            .range([0, size[1]])
+            .domain([lat1, lat2]);
+
+        //Create our brush using our brushX and brushY scales.
+        brush = d3.svg.brush()
+            .x(brushX)
+            .y(brushY)
+            .on('brush', function () {
+                dispatch.brushing(brush);
+            });
     }
 
     // Create a center method to serve as both a getter, and a setter.
@@ -123,25 +207,214 @@ bdv.map = function module() {
         dispatch.stopsEnd();
     };
 
+    //Create our addBrush method.
+    exports.addBrush = function () {
+        svg.append('g')
+            .attr('class', 'brush')
+            .call(brush)
+            .selectAll('rect')
+            .attr('width', width);
+        return this;
+    };
+
     //Bind our custom events to the 'on' method of our function.
     d3.rebind(exports, dispatch, 'on');
 
     return exports;
 };
 
+bdv.radialHistogram = function module() {
+    var slices = 24, // 24 hours in a day.
+        innerRadius = 100, // Default inner radius
+        outerRadius = 300, // Default outer radius
+        innerScale = d3.scale.linear(), // Define a scale for sizes segments based on value. group, // Our empty group variable
+        dimension, // Our empty dimension variable.
+        offset = 50, // Label offset value.
+        lowerColor, // The color used for the minimum of our range
+        upperColor, // The color used for the maximum of our range
+        innerRange, // The lower bound for radius value
+        outerRange, // The upper bound for radius value
+        color = d3.scale.linear(); // Linear color scale used for the segments.
+
+    function chart(_selection) {
+        // If the innerRange is not defined, it equals the innerRadius.
+        innerRange = innerRange ? innerRange : innerRadius;
+
+        // If the outerRange is not defined, it equals the outerRadius.
+        outerRange = outerRange ? outerRange : outerRadius;
+
+        // Our d3 arc generator for the segments.
+
+        var arc = d3.svg.arc()
+            .innerRadius(function (d, i) {
+                return innerScale(d);
+            })
+            .outerRadius(function (d, i) {
+                return outerRadius;
+            })
+            .startAngle(function (d, i) {
+                return 2 * Math.PI * (i / slices);
+            })
+            .endAngle(function (d, i) {
+                return 2 * Math.PI * ((i + 1) / slices);
+            });
+
+        // Our d3 arc generator for the labels.
+
+        var label = d3.svg.arc()
+            .innerRadius(outerRadius + offset)
+            .outerRadius(outerRadius + offset)
+            .startAngle(function (d, i) {
+                return 2 * Math.PI * (i / slices);
+            })
+            .endAngle(function (d, i) {
+                return 2 * Math.PI * ((i + 1) / slices);
+            });
+
+        //The total number of records for the city
+        var totalRecords = dimension.group().all(),
+        //The total number of delays for they city.
+            totalDelays = group.all();
+
+        //Obtain the min and max for both totalRecords and totalDelays.
+        // if there are no records, set to zero.
+        var mintotalRecords = totalRecords.length ? +totalRecords[0].key : 0,
+            maxtotalRecords = totalRecords.length ? +totalRecords[totalRecords.length-1].key : 0,
+            mintotalDelays = totalDelays.length ? +totalDelays[0].key : 0,
+            maxtotalDelays = totalDelays.length ? +totalDelays[totalDelays.length-1].key : 0;
+
+        //We must always have an array of length 24. Inspect the totalRecords array,
+        // and totalDelays array and splice to the beginning and end as required.
+        for (i=0; i<mintotalRecords; i++) {
+            totalRecords.splice(i, 0, {key:i, value:0});
+        }
+
+        for(i=maxtotalRecords; i<24; i++){
+            totalRecords.splice(i, 0, {key:i, value:0});
+        }
+
+        for (i=0; i<mintotalDelays; i++) {
+            totalDelays.splice(i, 0, {key:i, value:0});
+        }
+
+        for(i=maxtotalDelays; i<24; i++){
+            totalDelays.splice(i, 0, {key:i, value:0});
+        }
+
+        //Get the min and max values for both totalRecords, and totalDelays. We
+        // will use this for our scales.
+        var totalRecordsMax = d3.max(totalRecords, function (d) {return d.value;}),
+            totalRecordsMin = d3.min(totalRecords, function (d) {return d.value;});
+
+        //Set the range and domain for our innerScale using the min and max from the totalRecords.
+        innerScale.range([outerRange, innerRange]).domain([totalRecordsMin, totalRecordsMax]);
+
+        //Set the color range similarly
+        color.range([lowerColor, upperColor]).domain([totalRecordsMin, totalRecordsMax]);
+
+        //Update our segments using the current data.
+        var arcs = _selection.selectAll('path')
+            .data(totalDelays)
+            .attr('d', function (d,i) {return arc(d.value,i);})
+            .attr('fill', function (d) {return color(d.value);})
+            .attr('stroke', 'black')
+            .attr('class', 'slice');
+
+        //Add any new segments using the current data.
+        arcs.enter().append('path')
+            .attr('d', function (d,i) {return arc(d.value,i);})
+            .attr('fill', function (d) {return color(d.value);})
+            .attr('class', 'slice')
+            .attr('stroke', 'black');
+
+        //Remove and extra segments.
+        arcs.exit().remove();
+
+        //Attach our mouseover event.
+        arcs.on('mouseover', mouseover);
+
+        //Add our labels.
+        var labels = _selection.selectAll('text')
+            .data(totalDelays).enter()
+            .append("text")
+            .attr("transform", function(d,i) { return "translate(" + label.centroid(d,i) + ")"; })
+            .attr("dy", ".35em")
+            .attr("text-anchor", "middle")
+            .text(function(d,i) { return i+1; });
+
+        //Remove center text on chart update. TODO: Better way??
+        _selection.selectAll('.centerText').remove();
+
+        //Add the center text for the chart.
+        var centerText = _selection.append('text')
+            .attr('text-anchor', 'middle')
+            .text('Mouse over a segment to see the total.')
+            .attr('class', 'centerText');
+
+        //On mouseover function to display segment total.
+        function mouseover (d) {
+            centerText.text('Total: ' + d.value);
+        }
+
+    }
+
+    chart.innerRadius = function (_innerRadius) {
+        if (!arguments.length)
+            return innerRadius;
+        innerRadius = _innerRadius;
+        return chart
+    };
+
+    chart.outerRadius = function (_outerRadius) {
+        if (!arguments.length) return outerRadius;
+        outerRadius = _outerRadius;
+        return chart
+    };
+
+    //Method to get/set the crossfilter group.
+    chart.group = function (_group) {
+        if (!arguments.length) return group;
+        group = _group;
+        return chart;
+    };
+
+    //Method to get/set the label offset.
+    chart.offset = function (_offset) {
+        if (!arguments.length) return offset;
+        offset = _offset;
+        return chart;
+    };
+
+    //Method to get/set the crossfilter dimension.
+    chart.dimension = function (_dimension) {
+        if (!arguments.length) return dimension;
+        dimension = _dimension;
+        return chart;
+    };
+
+    //Method to get/set the color range.
+    chart.colorRange = function (_array) {
+        if (!arguments.length) return [lowerColor, upperColor];
+        lowerColor = _array[0];
+        upperColor = _array[1];
+        return chart;
+    };
+
+    //Method to get/set the radial range/
+    chart.radialRange = function (_array) {
+        if (!arguments.length) return [innerRange, outerRange];
+        innerRange = _array[0];
+        outerRange = _array[1];
+        return chart;
+    };
+
+    return chart;
+};
+
 var zurichDataManager = bdv.dataManager(),
     sanFranciscoDataManager = bdv.dataManager(),
     genevaDataManager = bdv.dataManager();
 
-//zurichDataManager.loadCsvData('/data/zurich/zurich_delay.csv', function (d) {
-//    var timeFormat = d3.time.format('%Y-%m-%d %H:%M:%S %p');
-//    d.DELAY = +d.DELAY_MIN;
-//    delete d.DELAY_MIN;
-//    d.SCHEDULED = timeFormat.parse(d.SCHEDULED);
-//    d.LATITUDE = +d.LATITUDE;
-//    d.LONGITUDE = +d.LONGITUDE;
-//    d.LOCATION = [d.LONGITUDE, d.LATITUDE];
-//});
 //
 //sanFranciscoDataManager.loadCsvData('/data/san_francisco/san_francisco_delay.csv', function (d) {
 //    var timeFormat = d3.time.format('%Y-%m-%d %H:%M:%S %p');
@@ -172,8 +445,14 @@ var zurichDataManager = bdv.dataManager(),
 var width = 570,
     height = 500;
 
-//Zurich
+//Instantiate our radial module for each city.
+var zurichRadial = bdv.radialHistogram().colorRange(['lightblue', 'darkblue'])
+    .innerRadius(5)
+    .outerRadius(200)
+    .offset(15)
+    .radialRange([100,200]);
 
+//Zurich
 var zurichMap = bdv.map()
     .center([8.5390, 47.3687])
     .scale(900000)
@@ -184,6 +463,16 @@ d3.select('#zurich_map')
     .attr('height', height)
     .call(zurichMap);
 
+
+//Set up the DOM for each city for the radial chart.
+var zurichHist = d3.select('#zurich_hist')
+    .append('svg')
+    .attr('width', 470)
+    .attr('height', 400)
+    .append('g')
+    .attr('transform', 'translate(' + 470/2 + ',' + 400/2 + ')');
+
+
 //// Load the routes data and pass our drawRoutes method as the callback to be executed upon data load.
 zurichDataManager.loadgeoJSON('/data/zurich/routes_topo.json', zurichMap.drawRoutes);
 
@@ -193,6 +482,40 @@ zurichDataManager.loadgeoJSON('/data/zurich/routes_topo.json', zurichMap.drawRou
 zurichMap.on('routesEnd', function () {
     zurichDataManager.loadgeoJSON('/data/zurich/stops_geo.json', zurichMap.drawStops);
 });
+
+//Load our Zurich data, and supply the cleaning function.
+
+zurichDataManager.loadCsvData('/data/zurich/zurich_delay.csv', function (d) {
+    var timeFormat = d3.time.format('%Y-%m-%d %H:%M:%S %p');
+    d.DELAY = +d.DELAY_MIN;
+    delete d.DELAY_MIN;
+    d.SCHEDULED = timeFormat.parse(d.SCHEDULED);
+    d.LATITUDE = +d.LATITUDE;
+    d.LONGITUDE = +d.LONGITUDE;
+    d.LOCATION = [d.LONGITUDE, d.LATITUDE];
+});
+
+zurichMap.addBrush();
+//.on('brushing', function (brush) {
+//    console.log(brush.extent());
+//});
+
+zurichMap.on('brushing', function (brush) {
+    var filteredLocations = zurichDataManager.filterLocation(brush.extent()),
+        delaysByHourAndLocation = zurichDataManager.getDelays(filteredLocations);
+
+    //// Inspect the total number of events by hour;
+    //console.log(delaysByHourAndLocation[0].group().all());
+    //// Inspect the total number of delays by hour.
+    //console.log(delaysByHourAndLocation[1].all());
+
+    // Pass in our filtered delays to the radial histogram.
+    zurichRadial.group(delaysByHourAndLocation[1]).dimension(delaysByHourAndLocation[0]);
+
+    // Update radial chart with the new data.
+    zurichHist.call(zurichRadial);
+});
+
 
 // sanFrancisco
 var sanFranciscoMap = bdv.map()
@@ -227,6 +550,6 @@ d3.select('#geneva_map')
 
 genevaDataManager.loadgeoJSON('/data/geneva/routes_topo.json', genevaMap.drawRoutes);
 
-genevaMap.on('routesEnd', function(){
-   genevaDataManager.loadgeoJSON('/data/geneva/stops_geo.json', genevaMap.drawStops)
+genevaMap.on('routesEnd', function () {
+    genevaDataManager.loadgeoJSON('/data/geneva/stops_geo.json', genevaMap.drawStops)
 });
